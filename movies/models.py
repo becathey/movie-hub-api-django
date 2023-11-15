3a4d0b05afab5ad1b0c893ccf4dbb57641837c5f
Bill Cathey <billcathey@hotmail.com>
2023-11-15 11:49:42 -0500

    Create Movie model

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Movie(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=100, blank=True, default="")
+    director = models.CharField(max_length=100)
+    year = models.IntegerField()
+    image = models.CharField(max_length=50)
+    description = models.CharField(max_length=500)
+    genre = models.CharField(max_length=255)
+    rating = models.FloatField()
+
+    class Meta:
+        ordering = ["rating"]
+
+    def __str__(self):
+        return self.title
