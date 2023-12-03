c447da441ec7b8e5c72141865a8951426438aeb4
Bill Cathey <billcathey@hotmail.com>
2023-12-02 22:10:08 -0500

    Edit DATABASES setting

--- backend/settings.py
+++ backend/settings.py
@@ -83,6 +83,8 @@ DATABASES = {
         conn_max_age=600
     )
 }
+database_url = os.environ.get("DATABASE_URL")
+DATABASES["default"] = dj_database_url.parse(database_url)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
